03f9bb43ec685e9ec720a97019e1782af03b4b52
alevelasco235 <utm24090891@utma.edu.mx>
2026-05-27 12:08:21 -0600

    Delete backend directory

--- backend/controllers/alumnos.controllers.js
+++
@@ -1,8 +0,0 @@
-import { Tabla } from "../models/alumnos.model.js";
-Tabla.create({
-    nombre : "coffe",
-    edad : 20,
-    materia : "Todas"
-
-})
-export const test =()=>console.log("Llamando el controladoren app")
--- backend/models/alumnos.model.js
+++
@@ -1,9 +0,0 @@
-import {Schema, model} from 'mongoose'
-
-
-const Esqueleto_de_tabla = new Schema({
-    nombre : String,
-    edad : Number,
-    materia : String
-})
-export const Tabla = new model("Tabla de alumnos reprobados", Esqueleto_de_tabla)
